112233927209ca26e19c3074f26249dfa555c9c6
Norbert Tagscherer <tagscherer.norbert@gmail.com>
2022-11-20 19:04:25 +0100

    add Comment entity

---
+++ src/DomainServices/DTOs/CommentDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingLikeFiles.DomainServices.DTOs
+{
+    class CommentDTO
+    {
+        public int Id { get; set; }
+        public int CaffId { get; set; }
+        public CaffDTO Caff { get; set; }
+        public int UserId { get; set; }
+    }
+}
---
+++ src/DomainServices/DataAccessLogic/Configurations/CommentConfiguration.cs
@@ -0,0 +1,35 @@
+﻿using DataAccessLogic.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLogic.Configurations
+{
+    public class CommentConfigration : IEntityTypeConfiguration<Comment>
+    {
+        public void Configure(EntityTypeBuilder<Comment> builder)
+        {
+            builder.Property(x => x.Text)
+                //.HasMaxLength(256)
+                .IsUnicode(false)
+                .IsRequired();
+
+            builder.HasKey(x => new { x.CaffId, x.UserId });
+
+            builder.HasOne(x => x.Caff)
+                .WithMany(x => x.Tags)
+                .HasForeignKey(x => x.CaffId)
+                .HasConstraintName($"fk{nameof(Comment)}To{nameof(Caff)}")
+                .OnDelete(DeleteBehavior.ClientCascade);
+
+           /* builder.HasOne(x => x.User)
+                .HasForeignKey(x => x.UserId)
+                .HasConstraintName($"fk{nameof(Comment)}To{nameof(User)}")
+                .OnDelete(DeleteBehavior.ClientCascade);*/
+        }
+    }
+}
