4523a56908469afe3a48d636f358f8785c5b2128
DAbraham2 <daniel.abraham@edu.bme.hu>
2022-11-30 23:28:20 +0100

    basic implementation
    testing to be done

--- src/DomainServices/Core/Internal/DefaultCaffValidator.cs
+++ src/DomainServices/Core/Internal/DefaultCaffValidator.cs
@@ -1,10 +1,57 @@
-﻿namespace ShoppingLikeFiles.DomainServices.Core.Internal;
+﻿using CliWrap;
+using Microsoft.Extensions.Options;
+using ShoppingLikeFiles.DomainServices.Options;
+
+namespace ShoppingLikeFiles.DomainServices.Core.Internal;
 
 internal class DefaultCaffValidator : ICaffValidator
 {
+    private readonly string _validator;
+
+    public DefaultCaffValidator(IOptions<CaffValidatorOptions> options)
+    {
+        if (options == null)
+        {
+            throw new ArgumentNullException(nameof(options));
+        }
+
+        _validator = options.Value.Validator;
+    }
+
+    internal DefaultCaffValidator(string validator)
+    {
+        _validator = validator;
+    }
+
     public bool ValidateFile(string fileName)
     {
         throw new NotImplementedException();
+    }
+
+    public async Task<bool> ValidateFileAsync(string fileName)
+    {
+        string cleanFileName = fileName.Trim();
+        if (string.IsNullOrEmpty(cleanFileName))
+        {
+            throw new ArgumentNullException(nameof(fileName));
+        }
+
+        Stream output = new MemoryStream();
+        await Cli.Wrap(_validator)
+            .WithArguments(fileName)
+            .WithStandardOutputPipe(PipeTarget.ToStream(output, true))
+            .ExecuteAsync();
+
+        var response = output.ReadByte();
+
+        output.Close();
+
+        if (response == 1)
+        {
+            return true;
+        }
+
+        return false;
     }
 }
 
