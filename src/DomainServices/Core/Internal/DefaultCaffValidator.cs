35c61cc7ead1378bcd552fd2a8b2c43387393ecd
DAbraham2 <daniel.abraham@edu.bme.hu>
2022-12-01 17:53:32 +0100

    implementation is tested
    Added unit tests
    Fixed implementation isue
    Added example runner (fresh build from NativeComponenet)

--- src/DomainServices/Core/Internal/DefaultCaffValidator.cs
+++ src/DomainServices/Core/Internal/DefaultCaffValidator.cs
@@ -1,16 +1,24 @@
 ﻿using CliWrap;
 using Microsoft.Extensions.Options;
 using ShoppingLikeFiles.DomainServices.Options;
+using System.Diagnostics;
+using System.Text;
 
 namespace ShoppingLikeFiles.DomainServices.Core.Internal;
 
 internal class DefaultCaffValidator : ICaffValidator
 {
     private readonly string _validator;
+    private const string validateArgument = "--validate";
 
     public DefaultCaffValidator(IOptions<CaffValidatorOptions> options)
     {
         if (options == null)
+        {
+            throw new ArgumentNullException(nameof(options));
+        }
+
+        if (string.IsNullOrEmpty(options.Value.Validator))
         {
             throw new ArgumentNullException(nameof(options));
         }
@@ -20,12 +28,44 @@ internal class DefaultCaffValidator : ICaffValidator
 
     internal DefaultCaffValidator(string validator)
     {
+        if (string.IsNullOrEmpty(validator))
+        {
+            throw new ArgumentNullException(nameof(validator));
+        }
+
         _validator = validator;
     }
 
     public bool ValidateFile(string fileName)
     {
-        throw new NotImplementedException();
+        string cleanFileName = fileName.Trim();
+        if (string.IsNullOrEmpty(cleanFileName))
+        {
+            throw new ArgumentNullException(nameof(fileName));
+        }
+        var arguments = GetArguments(cleanFileName);
+        using Process process = new();
+        process.StartInfo.FileName = _validator;
+        process.StartInfo.Arguments = arguments;
+        process.StartInfo.UseShellExecute = false;
+        process.StartInfo.RedirectStandardOutput = true;
+        bool result = process.Start();
+
+        if (!result)
+        {
+            return result;
+        }
+        StreamReader reader = process.StandardOutput;
+        var line = reader.ReadLine();
+
+        process.WaitForExit();
+
+        if (line == "1")
+        {
+            return true;
+        }
+
+        return false;
     }
 
     public async Task<bool> ValidateFileAsync(string fileName)
@@ -36,22 +76,22 @@ internal class DefaultCaffValidator : ICaffValidator
             throw new ArgumentNullException(nameof(fileName));
         }
 
-        Stream output = new MemoryStream();
+        var output = new StringBuilder();
         await Cli.Wrap(_validator)
-            .WithArguments(fileName)
-            .WithStandardOutputPipe(PipeTarget.ToStream(output, true))
+            .WithArguments(GetArguments(cleanFileName))
+            .WithStandardOutputPipe(PipeTarget.ToStringBuilder(output))
             .ExecuteAsync();
 
-        var response = output.ReadByte();
+        var response = output.ToString().Trim();
 
-        output.Close();
-
-        if (response == 1)
+        if (response == "1")
         {
             return true;
         }
 
         return false;
     }
+
+    private string GetArguments(string filename) => $"{filename} {validateArgument}";
 }
 
---
+++ test/DomainServices.UnitTest/CaffValidatorUnitTest.cs
@@ -0,0 +1,200 @@
+﻿using FluentAssertions.Extensions;
+using ShoppingLikeFiles.DomainServices.Core;
+using ShoppingLikeFiles.DomainServices.Core.Internal;
+
+namespace DomainServices.UnitTest;
+
+public class CaffValidatorUnitTest
+{
+    public class CaffValidatorCtorUnitTests
+    {
+        [Fact]
+        public void Test_ShouldThrow_ArgumentNull_1()
+        {
+            ICaffValidator validator;
+            Action action = () => validator = new DefaultCaffValidator("");
+
+            action.Should().Throw<ArgumentNullException>();
+        }
+
+        [Fact]
+        public void Test_ShouldThrow_ArgumentNull_2()
+        {
+            string? arg = null;
+            ICaffValidator validator;
+            Action action = () => validator = new DefaultCaffValidator(arg);
+
+            action.Should().Throw<ArgumentNullException>();
+
+        }
+    }
+
+    public class CaffValidatorSyncUnitTest
+    {
+        private string validatorPath()
+        {
+            var cwd = Directory.GetCurrentDirectory();
+
+            var files = Directory.EnumerateFiles(cwd, "CAFF_Proc*");
+
+            if (System.Runtime.InteropServices.RuntimeInformation.IsOSPlatform(osPlatform: System.Runtime.InteropServices.OSPlatform.Windows))
+            {
+                if (files.Any(f => f.EndsWith(".exe")))
+                {
+                    return files.First(f => f.EndsWith(".exe"));
+                }
+
+                throw new Exception("Validator not found!");
+            }
+
+            var file = files.FirstOrDefault(f => !f.EndsWith(".exe"));
+
+            if (string.IsNullOrEmpty(file))
+            {
+                throw new Exception("Validator not found!");
+            }
+
+            return file;
+            //return "Z:\\BME\\MSc\\SzamBiz\\ShoppingLikeFiles-NativeComponent\\cmake-build-debug\\CAFF_Processor.exe";
+        }
+
+
+        [Fact]
+        public void Test1()
+        {
+            ICaffValidator validator = new DefaultCaffValidator(validatorPath());
+
+            Action action = () => validator.ValidateFile("");
+
+            action.Should().Throw<ArgumentNullException>();
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            ICaffValidator validator = new DefaultCaffValidator(validatorPath());
+
+            bool result = validator.ValidateFile("notexistingfile.caff");
+
+            result.Should().Be(false, "Nonexistant file should not be valid");
+        }
+
+        [Fact]
+        public void Test3()
+        {
+            ICaffValidator validator = new DefaultCaffValidator(validatorPath());
+            var fileName = GetFile("invalid.caff");
+            bool result = validator.ValidateFile(fileName);
+
+            result.Should().Be(false, "Invalid file should not be valid");
+        }
+
+        [Fact]
+        public void Test4()
+        {
+            ICaffValidator validator = new DefaultCaffValidator(validatorPath());
+
+            var fileName = GetFile("validfile.caff");
+            bool result = validator.ValidateFile(fileName);
+
+            result.Should().Be(true, "A valid file should not be invalid");
+        }
+
+        private string GetFile(string testname)
+        {
+            var cwd = Directory.GetCurrentDirectory();
+
+            var files = Directory.EnumerateFiles(cwd, "*.caff");
+
+            if (files.Any(f => f.EndsWith(testname)))
+            {
+                return files.First(f => f.EndsWith(testname));
+            }
+
+            return "";
+        }
+    }
+
+    public class CaffValidatorAsyncUnitTest
+    {
+        private string validatorPath()
+        {
+            var cwd = Directory.GetCurrentDirectory();
+
+            var files = Directory.EnumerateFiles(cwd, "CAFF_Proc*");
+
+            if (System.Runtime.InteropServices.RuntimeInformation.IsOSPlatform(osPlatform: System.Runtime.InteropServices.OSPlatform.Windows))
+            {
+                if (files.Any(f => f.EndsWith(".exe")))
+                {
+                    return files.First(f => f.EndsWith(".exe"));
+                }
+
+                throw new Exception("Validator not found!");
+            }
+
+            var file = files.FirstOrDefault(f => !f.EndsWith(".exe"));
+
+            if (string.IsNullOrEmpty(file))
+            {
+                throw new Exception("Validator not found!");
+            }
+
+            return file;
+        }
+
+        [Fact]
+        public async Task Test1()
+        {
+            ICaffValidator validator = new DefaultCaffValidator(validatorPath());
+
+            Func<Task> act = () => validator.ValidateFileAsync("");
+
+            await act.Should().ThrowExactlyAsync<ArgumentNullException>();
+        }
+
+        [Fact]
+        public async Task Test2()
+        {
+            ICaffValidator validator = new DefaultCaffValidator(validatorPath());
+
+            Func<Task<bool>> act = () => validator.ValidateFileAsync("nonexisting.caff");
+
+            await act.Should().CompleteWithinAsync(500.Milliseconds()).WithResult(false);
+        }
+
+        [Fact]
+        public async Task Test3()
+        {
+            ICaffValidator validator = new DefaultCaffValidator(validatorPath());
+
+            Func<Task<bool>> act = () => validator.ValidateFileAsync(GetFile("invalid.caff"));
+
+            await act.Should().CompleteWithinAsync(500.Milliseconds()).WithResult(false);
+        }
+
+        [Fact]
+        public async Task Test4()
+        {
+            ICaffValidator validator = new DefaultCaffValidator(validatorPath());
+
+            Func<Task<bool>> act = () => validator.ValidateFileAsync(GetFile("validfile.caff"));
+
+            await act.Should().CompleteWithinAsync(500.Milliseconds()).WithResult(true);
+        }
+    }
+
+    internal static string GetFile(string testname)
+    {
+        var cwd = Directory.GetCurrentDirectory();
+
+        var files = Directory.EnumerateFiles(cwd, "*.caff");
+
+        if (files.Any(f => f.EndsWith(testname)))
+        {
+            return files.First(f => f.EndsWith(testname));
+        }
+
+        return "";
+    }
+}
